e55f0c21a7b6060cdd7d099fdb9ebe40e781d7fb
Luis Naranjo <luisnaranjo733@gmail.com>
2016-11-22 13:34:19 -0800

    Add AppBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+      <AppBar
+        title="Typing Frenzy Experiment"
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+        showMenuIconButton={false}
+      />
+      </MuiThemeProvider>
     );
   }
 }
